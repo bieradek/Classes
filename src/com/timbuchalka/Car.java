19c1dd979723dc3e4cd243db5c474494d6d2e1cf
bieradek <radoslaw.biedrzycki@syncron.com>
2018-08-08 17:30:46 +0200

    Uploading Car class with STATE component i.e. basic characteristics

---
+++ src/com/timbuchalka/Car.java
@@ -0,0 +1,10 @@
+package com.timbuchalka;
+
+public class Car {
+    // characteristics of a car, the STATE of the car
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+}
